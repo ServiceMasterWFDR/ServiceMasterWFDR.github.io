8e2bddcaa4ad6b7c4d17669eea972ed7ec88dc6d
olive02019 <bwat3513@gmail.com>
2025-02-24 18:14:15 -0600

    Update save_choices.php

--- save_choices.php
+++ save_choices.php
@@ -3,16 +3,37 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $choice1 = $_POST["choice1"];
-    $choice2 = $_POST["choice2"];
 
-    $data = "Choice 1: " . $choice1 . "\nChoice 2: " . $choice2 . "\n";
 
-    $filename = "user_choices.txt";
-    file_put_contents($filename, $data, FILE_APPEND);
+$file_url = "user_choices.txt";
 
-    echo "Choices saved.";
+$file_handle = fopen($file_url, 'r');
+
+
+
+if ($file_handle) {
+
+    while (($line = fgets($file_handle)) !== false) {
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $choice1 = $_POST["choice1"];
+            $choice2 = $_POST["choice2"];
+
+            $data = "Choice 1: " . $choice1 . "\nChoice 2: " . $choice2 . "\n";
+
+            $filename = "user_choices.txt";
+            file_put_contents($filename, $data, FILE_APPEND);
+
+            echo "Choices saved.";
+        } else {
+            echo "Invalid request.";
+        }
+        echo $line;
+    }
+
+    fclose($file_handle);
 } else {
-    echo "Invalid request.";
+
+    echo "Error opening file";
 }
+?>
